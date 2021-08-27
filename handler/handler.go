dd89880c190af89a781c99bf549037538180e97a
Rayhan Mahardhika Wijaya <rayhanmahardhika2000@gmail.com>
2021-08-27 20:24:10 +0700

    make layout for views and slices of struct

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"fmt"
+	"golangweb/entity"
 	"html/template"
 	"log"
 	"net/http"
@@ -20,22 +20,44 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	// w.Write([]byte("Welcome to home"))
 	// menggunakan template untuk menampilkan HTML
-	tmpl, err := template.ParseFiles(path.Join("views", "index.html"))
+	tmpl, err := template.ParseFiles(path.Join("views", "index.html"), path.Join("views", "layout.html"))
 	if err != nil {
 		// dev mode error
 		log.Println(err.Error())
 		// user friendly error
 		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
-		return 
+		return
 	}
 
-	err = tmpl.Execute(w, nil)
+	// menggunakan slices of struct sebagai parsing data ke views
+	data := []entity.Product{
+		{
+			ID : 1,
+			Name : "Mobilio",
+			Price : 220000000,
+			Stock : 3,
+		},
+		{
+			ID : 2,
+			Name : "Xpander",
+			Price : 270000000,
+			Stock : 2,
+		},
+		{
+			ID : 3,
+			Name : "Pajero Sport",
+			Price : 400000000,
+			Stock : 1,
+		},
+	}
+
+	err = tmpl.Execute(w, data)
 	if err != nil {
 		// dev mode error
 		log.Println(err.Error())
 		// user friendly error
 		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
-		return 
+		return
 	}
 
 }
@@ -59,6 +81,27 @@ func ProductHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// w.Write([]byte("Product Page : " + id))
+	// interface seperti ini memungkinkan bisa mengisi value dengan berbagai type data
+	data := map[interface{}]interface{}{
+		"content": idNumb,
+	}
 
-	fmt.Fprintf(w, "Product page : %d", idNumb)
+	// menggunakan template untuk menampilkan HTML
+	tmpl, err := template.ParseFiles(path.Join("views", "product.html"), path.Join("views", "layout.html"))
+	if err != nil {
+		// dev mode error
+		log.Println(err.Error())
+		// user friendly error
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		// dev mode error
+		log.Println(err.Error())
+		// user friendly error
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		return
+	}
 }
